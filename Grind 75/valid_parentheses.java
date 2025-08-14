a04cd02f347e165558ed0672908171bf987e9d5d
adnan106 <mdrakibulhasanadnan@gmail.com>
2025-08-14 19:33:38 +0600

    grind_75

---
+++ Grind 75/valid_parentheses.java
@@ -0,0 +1,37 @@
+import java.util.Stack;
+
+class valid_parentheses{
+    public static void main(String[] args){
+        System.out.println(new valid_parentheses().isValid("("));
+    }
+    public boolean isValid(String s) {
+        Stack<Character>st = new Stack<>();
+        for(int i=0;i<s.length();i++){
+            if(s.charAt(i)==')'){
+                if (st.empty() || st.peek()!='('){
+                    return false;
+                }
+                st.pop();
+            }
+            else if(s.charAt(i)==']'){
+                if(st.empty() || st.peek()!='['){
+                    return false;
+                }
+                st.pop();
+            }
+            else if(s.charAt(i)=='}'){
+                if(st.empty() || st.peek()!='{'){
+                    return false;
+                }
+                st.pop();
+            }
+            else{
+                st.push(s.charAt(i));
+            }
+        }
+        if(st.empty()){
+            return true;
+        }
+        return false;
+    }
+}
